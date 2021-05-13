bd772dfae0f56f0f7763a8b9446bffcb8c027f8b
Pardal <marcosnascimento@eng.ci.ufpb.br>
2021-05-13 11:08:45 -0300

    Add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,17 @@ import 'package:contacts/ui/home_page.dart';
 void main() {
   runApp(new MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.pink[800],
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.pink[800]),
+              borderRadius: BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.pink[800])),
+          hintStyle: TextStyle(color: Colors.pink[800])),
+    ),
+    debugShowCheckedModeBanner: false,
   ));
 }
