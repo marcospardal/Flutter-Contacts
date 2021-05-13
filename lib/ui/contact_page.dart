3672c7599cef91e91fdeca709ac2c54a6e19684d
Pardal <marcosnascimento@eng.ci.ufpb.br>
2021-05-13 19:09:20 -0300

    add discard changes message

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -52,181 +52,223 @@ class _ContactPageState extends State<ContactPage> {
     }
   }
 
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text(
+                "Discard Changes?",
+                style: TextStyle(
+                    color: Colors.pink[800], fontFamily: 'RobotoSlab'),
+              ),
+              content: Text("If you leave now, all changes will be lost."),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(color: Colors.pink[800]),
+                    )),
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    color: Colors.pink[800],
+                    child: Text(
+                      "Leave",
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink[800],
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text(
-          _editedContact.name ?? 'New Contact',
-          style: TextStyle(color: Colors.white, fontFamily: 'RobotoSlab'),
-        ),
-        centerTitle: true,
-      ),
-      floatingActionButton: Container(
-        height: 50,
-        width: 100,
-        child: FloatingActionButton(
-          onPressed: () {
-            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-              Navigator.pop(context, _editedContact);
-            } else {
-              FocusScope.of(context).requestFocus(_nameFocus);
-            }
-          },
-          child: Text(
-            "Save",
-            style: TextStyle(
-                color: Colors.white, fontFamily: 'RobotoSlab', fontSize: 20),
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.pink[800],
+            iconTheme: IconThemeData(color: Colors.white),
+            title: Text(
+              _editedContact.name ?? 'New Contact',
+              style: TextStyle(color: Colors.white, fontFamily: 'RobotoSlab'),
+            ),
+            centerTitle: true,
           ),
-          backgroundColor: Colors.pink[800],
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(15.0))),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                height: 250,
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    image: DecorationImage(
-                        image: _editedContact.image != null
-                            ? FileImage(File(_editedContact.image))
-                            : AssetImage("images/user.png"))),
+          floatingActionButton: Container(
+            height: 50,
+            width: 100,
+            child: FloatingActionButton(
+              onPressed: () {
+                if (_editedContact.name != null &&
+                    _editedContact.name.isNotEmpty) {
+                  Navigator.pop(context, _editedContact);
+                } else {
+                  FocusScope.of(context).requestFocus(_nameFocus);
+                }
+              },
+              child: Text(
+                "Save",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'RobotoSlab',
+                    fontSize: 20),
               ),
+              backgroundColor: Colors.pink[800],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
             ),
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                controller: _nameController,
-                focusNode: _nameFocus,
-                decoration: InputDecoration(
-                  labelText: "Name",
-                  labelStyle: TextStyle(
-                      color: Colors.pink[800], fontFamily: 'RobotoSlab'),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                onChanged: (text) {
-                  _userEdited = true;
-                  setState(() {
-                    _editedContact.name = text;
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  labelStyle: TextStyle(
-                      color: Colors.pink[800], fontFamily: 'RobotoSlab'),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                onChanged: (text) {
-                  _userEdited = true;
-                  _editedContact.email = text;
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                controller: _phoneController,
-                decoration: InputDecoration(
-                  labelText: "Phone",
-                  labelStyle: TextStyle(
-                      color: Colors.pink[800], fontFamily: 'RobotoSlab'),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                onChanged: (text) {
-                  _userEdited = true;
-                  _editedContact.phone = text;
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(15),
+            child: Column(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 5, right: 5),
-                  child: IconButton(
-                      icon: Icon(Icons.work),
-                      onPressed: () {
-                        setState(() {
-                          _editedContact.icon = 'work';
-                        });
-                      },
-                      iconSize: 40,
-                      color: _editedContact.icon == 'work'
-                          ? Colors.pink[800]
-                          : Colors.grey),
+                GestureDetector(
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                            image: _editedContact.image != null
+                                ? FileImage(File(_editedContact.image))
+                                : AssetImage("images/user.png"))),
+                  ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: 5, right: 5),
-                  child: IconButton(
-                      icon: Icon(Icons.people),
-                      onPressed: () {
-                        setState(() {
-                          _editedContact.icon = 'people';
-                        });
-                      },
-                      iconSize: 40,
-                      color: _editedContact.icon == 'people'
-                          ? Colors.pink[800]
-                          : Colors.grey),
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    controller: _nameController,
+                    focusNode: _nameFocus,
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      labelStyle: TextStyle(
+                          color: Colors.pink[800], fontFamily: 'RobotoSlab'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                        _userEdited = true;
+                        _editedContact.name = text;
+                      });
+                    },
+                  ),
                 ),
                 Padding(
-                    padding: EdgeInsets.only(left: 5, right: 5),
-                    child: IconButton(
-                        icon: Icon(Icons.family_restroom),
-                        onPressed: () {
-                          setState(() {
-                            _editedContact.icon = 'family';
-                          });
-                        },
-                        iconSize: 40,
-                        color: _editedContact.icon == 'family'
-                            ? Colors.pink[800]
-                            : Colors.grey)),
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                          color: Colors.pink[800], fontFamily: 'RobotoSlab'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedContact.email = text;
+                    },
+                  ),
+                ),
                 Padding(
-                  padding: EdgeInsets.only(left: 5, right: 5),
-                  child: IconButton(
-                      icon: Icon(Icons.school),
-                      onPressed: () {
-                        setState(() {
-                          _editedContact.icon = 'school';
-                        });
-                      },
-                      iconSize: 40,
-                      color: _editedContact.icon == 'school'
-                          ? Colors.pink[800]
-                          : Colors.grey),
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    controller: _phoneController,
+                    decoration: InputDecoration(
+                      labelText: "Phone",
+                      labelStyle: TextStyle(
+                          color: Colors.pink[800], fontFamily: 'RobotoSlab'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedContact.phone = text;
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: IconButton(
+                          icon: Icon(Icons.work),
+                          onPressed: () {
+                            setState(() {
+                              _editedContact.icon = 'work';
+                            });
+                          },
+                          iconSize: 40,
+                          color: _editedContact.icon == 'work'
+                              ? Colors.pink[800]
+                              : Colors.grey),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: IconButton(
+                          icon: Icon(Icons.people),
+                          onPressed: () {
+                            setState(() {
+                              _editedContact.icon = 'people';
+                            });
+                          },
+                          iconSize: 40,
+                          color: _editedContact.icon == 'people'
+                              ? Colors.pink[800]
+                              : Colors.grey),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 5, right: 5),
+                        child: IconButton(
+                            icon: Icon(Icons.family_restroom),
+                            onPressed: () {
+                              setState(() {
+                                _editedContact.icon = 'family';
+                              });
+                            },
+                            iconSize: 40,
+                            color: _editedContact.icon == 'family'
+                                ? Colors.pink[800]
+                                : Colors.grey)),
+                    Padding(
+                      padding: EdgeInsets.only(left: 5, right: 5),
+                      child: IconButton(
+                          icon: Icon(Icons.school),
+                          onPressed: () {
+                            setState(() {
+                              _editedContact.icon = 'school';
+                            });
+                          },
+                          iconSize: 40,
+                          color: _editedContact.icon == 'school'
+                              ? Colors.pink[800]
+                              : Colors.grey),
+                    )
+                  ],
+                ),
+                Text(
+                  _getIconLabel(_editedContact.icon),
+                  style: TextStyle(
+                      color: Colors.pink[800],
+                      fontSize: 35,
+                      fontFamily: 'RobotoSlab'),
                 )
               ],
             ),
-            Text(
-              _getIconLabel(_editedContact.icon),
-              style: TextStyle(
-                  color: Colors.pink[800],
-                  fontSize: 35,
-                  fontFamily: 'RobotoSlab'),
-            )
-          ],
+          ),
         ),
-      ),
-    );
+        onWillPop: _requestPop);
   }
 }
